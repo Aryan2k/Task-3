23c018d390df2689bfb044e917e9f688991a9cee
Aryan <aryanm468@gmail.com>
2023-07-13 11:46:08 +0530

    Implementation of Linear progress bar

--- app/src/main/java/com/example/task3/MainActivity.kt
+++ app/src/main/java/com/example/task3/MainActivity.kt
@@ -3,16 +3,18 @@ package com.example.task3
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.LinearProgressIndicator
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.getValue
+import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalLifecycleOwner
@@ -25,6 +27,9 @@ import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
+
+    private lateinit var a: ProgressBar
+    private lateinit var b: ProgressBar
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -33,48 +38,69 @@ class MainActivity : ComponentActivity() {
             }
         }
     }
-}
 
-@Composable
-fun DrawUI() {
+    @Composable
+    fun DrawUI() {
 
-    var progress by remember { mutableStateOf(0.0f) }
+        Row(Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.SpaceAround) {
 
-    val duration = 30 * 60 // 30 minutes in seconds
-    val lifecycleOwner = LocalLifecycleOwner.current
+            a = ProgressBar(remember {
+                mutableStateOf(0f)
+            })
 
-    LaunchedEffect(key1 = lifecycleOwner) {
-        lifecycleOwner.lifecycle.repeatOnLifecycle(Lifecycle.State.STARTED) {
-            launch {
-                repeat(duration) { iteration ->
-                    progress = (iteration + 1).toFloat() / duration
-                    delay(1000) // Delay for 1 second
+            b = ProgressBar(remember {
+                mutableStateOf(-1f)
+            })
+
+            a.LoadProgressBar()
+            b.LoadProgressBar()
+
+        }
+    }
+
+    inner class ProgressBar(var progress: MutableState<Float>) {
+
+        @Composable
+        fun LoadProgressBar() {
+
+            LinearProgressIndicator(
+                modifier = Modifier
+                    .height(200.dp)
+                    .width(150.dp)
+                    .padding(16.dp),
+                progress = if (progress.value >= 0f) progress.value else 0f,
+                color = Color.Red,
+                trackColor = Color.LightGray
+            )
+
+            if (progress.value >= 0f) {
+                val duration = 5 // 30 minutes in seconds
+                val lifecycleOwner = LocalLifecycleOwner.current
+
+                LaunchedEffect(key1 = lifecycleOwner) {
+                    lifecycleOwner.lifecycle.repeatOnLifecycle(Lifecycle.State.STARTED) {
+                        launch {
+                            repeat(duration) { iteration ->
+                                progress.value = (iteration + 1).toFloat() / duration
+                                delay(1000) // Delay for 1 second
+
+                                if (a.progress.value == 1f) {
+                                    a.progress.value = -1f
+                                    b.progress.value = 0f
+                                }
+                            }
+                        }
+                    }
                 }
             }
         }
     }
 
-    LoadProgressBar(progress = progress)
-}
-
-@Composable
-fun LoadProgressBar(progress: Float) {
-    LinearProgressIndicator(
-        modifier = Modifier
-            .height(200.dp)
-            .fillMaxWidth()
-            .padding(16.dp),
-        progress = progress,
-        color = Color.Red,
-        trackColor = Color.LightGray
-    )
-}
-
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    Task3Theme {
-        DrawUI()
+    @Preview(showBackground = true)
+    @Composable
+    fun GreetingPreview() {
+        Task3Theme {
+            DrawUI()
+        }
     }
 }
